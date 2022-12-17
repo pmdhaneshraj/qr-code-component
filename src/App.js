4e9821d4f7f1d9cbb3d541d185574c43569bb9d2
dhaneshtekion <dhaneshr@tekion.com>
2022-12-17 16:36:27 +0530

    Designed for desktop view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Card } from 'antd';
+
+import './App.scss';
+
+import qrImg from './assests/images/image-qr-code.png'
+
+const { Meta } = Card;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App pattern-zigzag-xl bg-blue white">
+      <div className='qr-container'>
+        <div className='qr-card-container'>
+          <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={<img className='qr-img' alt="qr-img" src={qrImg} />}
+          >
+            <Meta 
+              title="Improve your front-end skills by building projects" 
+              description="scan QR code to visit Frontend Mentors and take your coding skills to next level"
+            />
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
